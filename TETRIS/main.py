dad7d649c8c3b6f5305ffea3973d3be508e4e54d
Danilo Ribeiro <154379041+Danil0Ribeir0@users.noreply.github.com>
2024-04-05 10:23:18 -0300

    Update main.py
    base do jogo e suas explicações

--- TETRIS/main.py
+++ TETRIS/main.py
@@ -1,16 +1,18 @@
 import pygame, sys
 
 pygame.init()
+color = (44, 44, 127)
 
-screen = pygame.display.set_mode((300, 600))
-pygame.display.set_caption("Python Tetris")
+screen = pygame.display.set_mode((300, 600)) # tamanho da janela (grid)
+pygame.display.set_caption("Python Tetris") # Título do jogo
 
-clock = pygame.time.Clock()
+clock = pygame.time.Clock() # inicia o loop para o jogo
 
 while True: 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT: # checa quando o jogo é fechado
             pygame.quit()
             sys.exit()
-    pygame.display.update
-    clock.tick(60)
+    screen.fill(color) # preenche a tela com a cor escolhida
+    pygame.display.update()
+    clock.tick(60) # determina a velocidade de atualização do jogo
